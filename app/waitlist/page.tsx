5dba1785468d2ef087c140d09f424f7a710119e0
iamk3nnyt <ktranish@outlook.com>
2025-09-26 22:06:03 +0200

    wip

---
+++ app/waitlist/page.tsx
@@ -0,0 +1,8 @@
+export default function WaitlistPage() {
+  return (
+    <>
+      <div data-tf-live="01K63T0QEKACADFRY383DDGQFA"></div>
+      <script src="//embed.typeform.com/next/embed.js"></script>
+    </>
+  );
+}
